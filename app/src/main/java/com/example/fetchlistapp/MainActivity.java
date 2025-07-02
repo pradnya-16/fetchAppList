df990b31e3090f8de72b6d7a08e694198f4789cf
pradnya-16 <kpradnya1601@gmail.com>
2025-07-01 22:29:45 -0500

    Final Commit

--- app/src/main/java/com/example/fetchlistapp/MainActivity.java
+++ app/src/main/java/com/example/fetchlistapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fetchlistapp.adapter.ItemAdapter;
@@ -24,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         adapter = new ItemAdapter();
